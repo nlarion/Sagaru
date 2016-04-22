183214ebd67db086a7da820c2fc86991c09a89a2
Neil Larion <neil.larion@gmail.com>
2016-04-22 09:33:37 -0700

    Get basic models setup and ready for code-first database development

---
+++ Sagaru/src/Sagaru/Models/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sagaru.Models
+{
+    public class Shape
+    {
+        [Key]
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
