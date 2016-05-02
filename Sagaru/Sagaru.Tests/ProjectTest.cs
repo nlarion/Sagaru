0c99c69103ffdddbe0308325b6a86bcf43ad1996
Neil Larion <neil.larion@gmail.com>
2016-05-01 23:04:56 -0700

    Add first test for project

---
+++ Sagaru/Sagaru.Tests/ProjectTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sagaru.Models;
+using Xunit;
+
+
+namespace Sagaru.Tests
+{
+    public class ProjectTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var project = new Project();
+            project.Description = "Wash the dog";
+
+            //Act
+            var result = project.Description;
+
+            //Assert
+            Assert.Equal("Wash the dog", result);
+        }
+    }
+}
